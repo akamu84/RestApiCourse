16e9900a993928f4f7aaab7e9f21a93bd4602d9a
akamu84 <hnviN=Wx_L@1%DT@>
2023-07-28 22:07:57 -0500

    Add SDK and SDK consumer

---
+++ Movies.Api.Sdk.Consumer/AuthTokenProvider.cs
@@ -0,0 +1,56 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Json;
+
+namespace Movies.Api.Sdk.Consumer;
+
+public class AuthTokenProvider
+{
+    private readonly HttpClient _httpClient;
+    
+    private string _cachedToken = string.Empty;
+    private static readonly SemaphoreSlim Lock = new(1, 1);
+    
+    public AuthTokenProvider(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetTokenAsync(CancellationToken token = default)
+    {
+        if (!string.IsNullOrEmpty(_cachedToken))
+        {
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(_cachedToken);
+            var expiryTimeText = jwt.Claims.Single(claim => claim.Type == "exp").Value;
+            var expiryTime = UnixTimeStampToDateTime(int.Parse(expiryTimeText));
+            if (expiryTime > DateTime.UtcNow)
+            {
+                return _cachedToken;
+            }
+        }
+        
+        await Lock.WaitAsync(token);
+
+        var response = await _httpClient.PostAsJsonAsync("https://localhost:5003/token", new
+            {
+                userid = "d8566de3-b1a6-4a9b-b842-8e3887a82e43",
+                email = "nick@nickchapsas.com",
+                customClaims = new Dictionary<string, object>
+                {
+                    { "admin", true },
+                    { "trusted_member", true }
+                }
+            }, cancellationToken: token);
+
+        var newToken = await response.Content.ReadAsStringAsync(token);
+        _cachedToken = newToken;
+        Lock.Release();
+        return newToken;
+    }
+    
+    private static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
+    {
+        var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+        return dateTime;
+    }
+}
---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
+using Refit;
+
+var services = new ServiceCollection();
+
+services.AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+    {
+        AuthorizationHeaderValueGetter = async (_, token) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync(token)
+    })
+    .ConfigureHttpClient(x => x.BaseAddress = new Uri("https://localhost:5001"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("nick-the-greek-2023");
+
+var movies = await moviesApi.GetMoviesAsync(new GetAllMoviesRequest
+{
+    Title = null,
+    YearOfRelease = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+});
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
+Console.WriteLine(JsonSerializer.Serialize(movies));
